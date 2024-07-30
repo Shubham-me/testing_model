57dcad4a00dc0ad90dd5e206a25ec86c09222771
Shubham <87982899+Shubham-me@users.noreply.github.com>
2024-07-31 00:38:35 +0530

    Update multiply.kt

--- app/src/main/java/com/example/operations/elementary/multiply.kt
+++ app/src/main/java/com/example/operations/elementary/multiply.kt
@@ -2,6 +2,10 @@ package com.example.operations.elementary
 fun multiply(a : Int,b : Int){
     // add b, a times to 0
     var ans : Int = 0
+    if(a < 0){
+        a = -a
+        b = -b
+    }
     for (i in 1..a){
         ans = add(ans,b)
     }
